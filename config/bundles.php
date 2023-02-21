96513adae8681966b30387072e649256ad677426
Jonathan Secher <secher.jonathan@gmail.com>
2023-02-21 16:53:47 +0100

    Revert "Merge pull request #12 from ashk74/feature/lazy-image"
    This reverts commit c631044fcbc36a2a5754d2b240b4076a473d25e5, reversing
    changes made to 2edd1001c2867b84e9c483a272c59566e69ef18d.

--- config/bundles.php
+++ config/bundles.php
@@ -7,5 +7,4 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
-    Symfony\UX\LazyImage\LazyImageBundle::class => ['all' => true],
 ];
